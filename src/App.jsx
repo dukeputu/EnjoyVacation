6cfff9f7e28f0b557ec5ea35f9bd78524d69c744
dukeputu <roysk0006@gmail.com>
2024-02-28 01:08:08 +0530

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+const EnjoyVacation = () => {
+  return (
+    <center>
+      <h1 className='text-5xl mt-9'>Enjoy Your Vacation 😎</h1>
+      <p className='text-3xl mt-9'> May your days be as peaceful as the ocean waves 🌊💙</p>
+      <h2 className='text-5xl mt-9'>❤</h2>
+    </center>
+  );
+}
+export default EnjoyVacation;
